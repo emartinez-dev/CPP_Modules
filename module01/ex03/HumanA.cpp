fa4b0171b8628d21866af13ce016477184f6418b
emartinez <enri.fc.98@gmail.com>
2023-05-08 11:06:16 +0200

    docs(m01/ex03): initialization list explanaition

--- module01/ex03/HumanA.cpp
+++ module01/ex03/HumanA.cpp
@@ -6,12 +6,17 @@
 /*   By: franmart <franmart@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/02 10:15:26 by franmart          #+#    #+#             */
-/*   Updated: 2023/05/03 10:40:31 by franmart         ###   ########.fr       */
+/*   Updated: 2023/05/08 11:05:50 by franmart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "HumanA.hpp"
 
+/* I initialized private variables inside classes like: "this->variable = value"
+before, but it's a better practice to initialize them with an initialization
+list. It's more efficient, the compiler directly initializes the member
+variable rather than creating a temporary object and then assigning it to the
+member variable */
 HumanA::HumanA( std::string name, Weapon &weapon ) :\
 	 _name(name), _weapon(weapon)
 {

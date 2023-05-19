7988a5d3e4d4fcee8a069a0b1e9ed6f2f356c635
emartinez <enri.fc.98@gmail.com>
2023-05-19 13:39:39 +0200

    feat(m02/ex03): bsp implemented

--- module02/ex03/bsp.cpp
+++ module02/ex03/bsp.cpp
@@ -6,34 +6,34 @@
 /*   By: franmart <franmart@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/18 18:03:55 by franmart          #+#    #+#             */
-/*   Updated: 2023/05/19 11:47:42 by franmart         ###   ########.fr       */
+/*   Updated: 2023/05/19 13:36:54 by franmart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 #include "Point.hpp"
 
-/* Different implementation: the barycentric coordinates method!
+/* Different implementation: the triangles area method!
 reference: https://www.baeldung.com/cs/check-if-point-is-in-2d-triangle
 */
 
 Fixed	triangle_area(Point const a, Point const b, Point const c)
 {
-	Fixed	area;
 	Point	ab(b.getX().toFloat() - a.getX().toFloat(), \
 		b.getY().toFloat() - a.getY().toFloat());
 	Point	ac(c.getX().toFloat() - a.getX().toFloat(), \
 		c.getY().toFloat() - a.getY().toFloat());
-	return Fixed(0.5f * (ab.getX().toFloat() * ac.getY().toFloat() - \
-		ab.getY().toFloat() * ac.getX().toFloat()));
+	Fixed	area(((ab.getX() * ac.getY() - ab.getY() * ac.getX()) / Fixed(2.0f)).toFloat());
+	if (area < 0)
+		return (area * -1);
+	return area;
 }
 
-bool bsp(Point const a, Point const b, Point const c, Point const point)
+bool	bsp(Point const a, Point const b, Point const c, Point const point)
 {
-	Fixed	area_abc;
+	Fixed	area_abc(triangle_area(a, b, c));
 	Fixed	area_sum(0.0f);
 
-	area_abc = triangle_area(a, b, c);
 	area_sum = area_sum + triangle_area(a, b, point);
 	area_sum = area_sum + triangle_area(a, c, point);
 	area_sum = area_sum + triangle_area(b, c, point);
--- module02/ex03/main.cpp
+++ module02/ex03/main.cpp
@@ -6,18 +6,20 @@
 /*   By: franmart <franmart@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/08 10:15:15 by franmart          #+#    #+#             */
-/*   Updated: 2023/05/19 11:45:54 by franmart         ###   ########.fr       */
+/*   Updated: 2023/05/19 13:39:04 by franmart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "bsp.hpp"
+
+/* Tool to represent points online: https://www.geogebra.org/graphing */
 
 int main(void)
 {
 	Point	a(0.0f, 0.0f);
 	Point	b(5.0f, 0.0f);
 	Point	c(2.5f, 2.5f);
-	Point	point(1.0f, 1.0f);
+	Point	point(1.0f, 0.5f);
 
 	std::cout << "Triangle vertices:" << std::endl;
 	std::cout << "a) x:" << a.getX() << " y:" << a.getY() << std::endl;

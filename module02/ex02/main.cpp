19d94c60004d6e1eb712e3e975c8290d61fec1c7
emartinez <enri.fc.98@gmail.com>
2023-06-25 14:55:36 +0200

    feat(m02/ex02): add more tests to main

--- module02/ex02/main.cpp
+++ module02/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: franmart <franmart@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/08 10:15:15 by franmart          #+#    #+#             */
-/*   Updated: 2023/05/10 13:36:07 by franmart         ###   ########.fr       */
+/*   Updated: 2023/06/25 14:55:02 by franmart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,12 +26,33 @@ int main(void) {
 	std::cout << "A - B: " << a - b << std::endl;
 	std::cout << "A * B: " << a * b << std::endl;
 	std::cout << "A / B: " << a / b << std::endl << std::endl;
+
+	a = 0.4f;
+	b = 0.5f;
+	std::cout << "A: "<< a << " | B: " << b << std::endl;
 	if (a > b)
 		std::cout << "A is bigger than B" << std::endl;
-	if (b > a)
-		std::cout << "B is bigger than A" << std::endl;
+	if (a < b)
+		std::cout << "A is smaller than B" << std::endl;
 
+	a = 0.5f;
+	b = 0.5f;
+	std::cout << "A: "<< a << " | B: " << b << std::endl;
+	if (a >= b)
+		std::cout << "A is bigger or equal to B" << std::endl;
+	if (a <= b)
+		std::cout << "A is smaller or equal to B" << std::endl;
+
+	if (a == b)
+		std::cout << "A is equal to B" << std::endl;
+	if (a != b)
+		std::cout << "A is not equal to B" << std::endl;
+
+	a = 206.5f;
+	b = 12.5f;
+	std::cout << "A: "<< a << " | B: " << b << std::endl;
 	std::cout << "Max between A and B: " << Fixed::max(a, b) << std::endl;
+	std::cout << "Min between A and B: " << Fixed::min(a, b) << std::endl;
 
 	return 0;
 }

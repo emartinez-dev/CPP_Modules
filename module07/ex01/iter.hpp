0a0badeb1e4930fb76e67628bc2a0bc94ae812b2
emartinez <enri.fc.98@gmail.com>
2023-08-21 09:46:25 +0200

    feat(m07/ex01): implement the iter template function

---
+++ module07/ex01/iter.hpp
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   iter.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: franmart <franmart@student.42malaga.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/21 08:24:22 by franmart          #+#    #+#             */
+/*   Updated: 2023/08/21 08:52:59 by franmart         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ITER_HPP
+#define ITER_HPP
+
+# include <iostream>
+
+template <typename T>
+void iter(T *array, int size, T (*f)(T))
+{
+	for (int i = 0; i < size; i++)
+		array[i] = f(array[i]);
+}
+
+template <typename T>
+void iter(T *array, int size, void (*f)(T))
+{
+	for (int i = 0; i < size; i++)
+		f(array[i]);
+}
+
+template <typename T>
+void print_element(T i)
+{
+	std::cout << i << ", ";
+}
+
+#endif

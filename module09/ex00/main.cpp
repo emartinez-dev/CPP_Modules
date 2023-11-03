1adb128723999d66c56d27d63b8d3aeb2efde1b2
emartinez <enri.fc.98@gmail.com>
2023-11-03 10:27:06 +0100

    fix(m09/ex00): catch the exception raised from having no DB

--- module09/ex00/main.cpp
+++ module09/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: franmart <franmart@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/29 11:57:13 by franmart          #+#    #+#             */
-/*   Updated: 2023/08/29 17:21:05 by franmart         ###   ########.fr       */
+/*   Updated: 2023/11/03 10:26:14 by franmart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,13 @@ int main (int argc, char **argv)
 		std::cout << "Error: Wrong number of arguments. Usage: ./btc [input_file]\n";
 		return (1);
 	}
-	BitcoinExchange exchange;
-	calculateExchange(argv[1], &exchange);
+	try {
+		BitcoinExchange exchange;
+		calculateExchange(argv[1], &exchange);
+	}
+	catch (std::exception &e)
+	{
+		std::cout << e.what() << std::endl;
+	}
 	return 0;
 }
